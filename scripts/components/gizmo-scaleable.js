cc7d2dc490373817a03dc2019819be4fd037642a
Vincent Davies <55065241+vsantino720@users.noreply.github.com>
2021-04-25 21:46:27 -0400

    Update gizmo-scaleable.js

--- scripts/components/gizmo-scaleable.js
+++ scripts/components/gizmo-scaleable.js
@@ -6,7 +6,7 @@
 AFRAME.registerComponent('gizmo-scaleable', {
   init: function() {
       this.el.sceneEl.addEventListener("cursor-move", (e) => {
-        let el = document.querySelector("[scaleable]") //Get the scaleable entity
+        let el = document.querySelector("[gizmo-scaleable]") //Get the scaleable entity
         this.el.object3D.scale.y = this.el.object3D.scale.y + e.detail.change;
         this.el.object3D.scale.x = this.el.object3D.scale.x + e.detail.change;
         this.el.object3D.scale.z = this.el.object3D.scale.z + e.detail.change;
